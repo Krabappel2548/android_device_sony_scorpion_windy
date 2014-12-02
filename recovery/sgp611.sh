e9f0f539673fcc764f0678afd632846be843f5a5
krabappel2548 <krabappel2548@gmail.com>
2014-12-02 01:37:36 +0100

    Fix some more naming stuff

--- recovery/sgp611.sh
+++ recovery/sgp611.sh
@@ -2,10 +2,10 @@
 
 mkdir -p /lta-label
 mount -o ro /dev/block/platform/msm_sdcc.1/by-name/LTALabel /lta-label
-ls lta-label/*.html | grep -q d6603
+ls lta-label/*.html | grep -q sgp611
 if [ $? -eq 0 ]; then
 	mount /dev/block/platform/msm_sdcc.1/by-name/system /system
-	cp /system/etc/firmware/d6603/* /system/etc/firmware/
+	cp /system/etc/firmware/sgp611/* /system/etc/firmware/
 	umount /system
 fi
 umount /lta-label
